33dbfe72b2aacffbdaccdee1b8d8b59ad6c6261a
John Buco <john.buco@loblaw.ca>
2024-07-03 16:27:06 -0400

    video support

--- modules/mabidderBidAdapter.js
+++ modules/mabidderBidAdapter.js
@@ -1,40 +1,41 @@
 import { registerBidder } from '../src/adapters/bidderFactory.js';
-import { BANNER } from '../src/mediaTypes.js';
+import { BANNER, VIDEO } from '../src/mediaTypes.js';
 import { getGlobal } from '../src/prebidGlobal.js';
 import { ortbConverter } from '../libraries/ortbConverter/converter.js';
+import { deepAccess } from '../src/utils.js';
 
 const BIDDER_CODE = 'mabidder';
 export const baseUrl = 'https://prebid.ecdrsvc.com/bid';
 const converter = ortbConverter({})
 
 export const spec = {
-  supportedMediaTypes: [BANNER],
+  supportedMediaTypes: [BANNER, VIDEO],
   code: BIDDER_CODE,
   isBidRequestValid: function(bid) {
     if (typeof bid.params === 'undefined') {
       return false;
     }
-    return !!(bid.params.ppid && bid.sizes && Array.isArray(bid.sizes) && Array.isArray(bid.sizes[0]))
+    return !!(bid.params.ppid && ((getSizes(bid).length > 0)))
   },
   buildRequests: function(validBidRequests, bidderRequest) {
     const fpd = converter.toORTB({ bidRequests: validBidRequests, bidderRequest: bidderRequest });
 
     const bids = [];
     validBidRequests.forEach(bidRequest => {
-      const sizes = [];
-      bidRequest.sizes.forEach(size => {
-        sizes.push({
-          width: size[0],
-          height: size[1]
-        });
-      });
-      bids.push({
+      const bid = {
         bidId: bidRequest.bidId,
-        sizes: sizes,
         ppid: bidRequest.params.ppid,
-        mediaType: BANNER
-      })
+        sizes: getSizes(bidRequest)
+      };
+      if (bidRequest.mediaType == BANNER || isBanner(bidRequest)) {
+        bid.mediaType = BANNER;
+      } else if (bidRequest.mediaType == VIDEO || isVideo(bidRequest)) {
+        bid.mediaType = VIDEO;
+        bid.video = deepAccess(bidRequest, 'mediaTypes.video');
+      }
+      bids.push(bid)
     });
+
     const req = {
       url: baseUrl,
       method: 'POST',
@@ -64,3 +65,38 @@ export const spec = {
   }
 }
 registerBidder(spec);
+
+function isVideo(bid) {
+  return !!deepAccess(bid, 'mediaTypes.video');
+}
+
+function isBanner(bid) {
+  return !!deepAccess(bid, 'mediaTypes.banner');
+}
+
+function getSizes(bidRequest) {
+  let sizes = [];
+  if (bidRequest.mediaType == BANNER || isBanner(bidRequest)) {
+    const banner = deepAccess(bidRequest, 'mediaTypes.banner');
+    banner.sizes.forEach(size => {
+      sizes.push({
+        width: size[0],
+        height: size[1]
+      });
+    });
+  } else if (bidRequest.mediaType == VIDEO || isVideo(bidRequest)) {
+    const video = deepAccess(bidRequest, 'mediaTypes.video')
+    sizes.push({
+      width: video.playerSize[0],
+      height: video.playerSize[1]
+    });
+  } else if (bidRequest.sizes && Array.isArray(bidRequest.sizes) && Array.isArray(bidRequest.sizes[0])) {
+    bidRequest.sizes.forEach(size => {
+      sizes.push({
+        width: size[0],
+        height: size[1]
+      });
+    });
+  }
+  return sizes;
+}
